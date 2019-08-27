dfb0df9353bc209ef7a1972505ed3d5ab7b38cbb
S. Fleming <nuxnik@int5.net>
2019-08-27 11:45:36 +0200

    Added query builder classes

---
+++ src/ApiQuery.js
@@ -0,0 +1,222 @@
+/**
+ * Class: ApiQuery
+ *
+ * Query object for JSON+Collection APIs
+ *
+ * @author S. Fleming <nuxnik@int5.net>
+ * @since Mon 26 Aug 2019 01:46:03 PM CEST
+ */
+export default class ApiQuery
+{
+  /**
+   * The ROOT type
+   *
+   * @return string
+   */
+  static get ROOT() {
+    return "ROOT";
+  }
+
+  /**
+   * The ITEM type
+   *
+   * @return string
+   */
+  static get ITEM() {
+    return "ITEM" 
+  }
+
+  constructor()
+  {
+    /**
+     * The query parameters
+     *
+     * @var Object
+     */
+    this.params = {};
+
+    this.index;
+    this.searchKey;
+    this.searchValue;
+    this.node;
+    this.type;
+  }
+  /**
+   * Add parameters to the query
+   *
+   * @param {Object} params The parameter object
+   * @since Mon 26 Aug 2019 02:01:12 PM CEST
+   * @return ApiQuery
+   */
+  addParams(params)
+  {
+    this.params = params;
+
+    return this;
+  }
+
+  /**
+   * getParamsAsString
+   *
+   * @since Mon 26 Aug 2019 02:04:35 PM CEST
+   * @return string
+   */
+  getParamsAsString()
+  {
+    let string = '';
+    for (var key in this.params) {
+      string += key + '=' + this.params[key] + "&";
+    }
+    return string.substring(0, string.length - 1);;
+  }
+
+  /**
+   * setIndex
+   *
+   * @param {number} index The item index to fetch
+   * @since Mon 26 Aug 2019 02:02:05 PM CEST
+   * @return ApiQuery
+   */
+  setIndex(index)
+  {
+    this.index = index;
+
+    return this;
+  }
+
+  /**
+   * getIndex
+   *
+   * @since Mon 26 Aug 2019 02:05:06 PM CEST
+   * @return number
+   */
+  getIndex()
+  {
+    return this.index;
+  }
+
+  /**
+   * hasIndex
+   *
+   * @since Mon 26 Aug 2019 02:05:13 PM CEST
+   */
+  hasIndex()
+  {
+    return this.index != undefined ? true : false;
+  }
+
+  /**
+   * search
+   *
+   * @since Mon 26 Aug 2019 02:02:48 PM CEST
+   * @return ApiQuery
+   */
+  search(key, value)
+  {
+    this.searchKey   = key;
+    this.searchValue = value;
+
+    return this;
+  }
+
+  /**
+   * getSearchKey
+   *
+   * @since Mon 26 Aug 2019 02:06:01 PM CEST
+   * @return string
+   */
+  getSearchKey()
+  {
+    return this.searchKey;
+  }
+
+  /**
+   * getSearchValue
+   *
+   * @since Mon 26 Aug 2019 02:06:12 PM CEST
+   * @return string
+   */
+  getSearchValue()
+  {
+    return this.searchValue;
+  }
+
+  /**
+   * isSearchable
+   *
+   * @since Mon 26 Aug 2019 02:05:37 PM CEST
+   * @return boolean
+   */
+  isSearchable()
+  {
+    return (this.searchValue != undefined || this.searchKey != undefined) ? true : false;
+  }
+
+  /**
+   * getNode
+   *
+   * @since Mon 26 Aug 2019 02:04:07 PM CEST
+   * @return string
+   */
+  getNode()
+  {
+    return this.node;
+  }
+
+  /**
+   * setNode
+   *
+   * @param string node The rel link name
+   * @since Mon 26 Aug 2019 02:04:12 PM CEST
+   * @return string
+   */
+  setNode(node)
+  {
+    this.node = node;
+
+    return this;
+  }
+
+  /**
+   * getType
+   *
+   * @since Mon 26 Aug 2019 02:27:50 PM CEST
+   * @return string:
+   */
+  getType()
+  {
+    return this.type;
+  }
+
+  /**
+   * setType
+   *
+   * @param {String} Set the type
+   * @since Mon 26 Aug 2019 02:28:07 PM CEST
+   */
+  setType(type)
+  {
+    this.type = type;
+    return this;
+  }
+
+  /**
+   * isRoot
+   *
+   * @since Mon 26 Aug 2019 03:21:15 PM CEST
+   */
+  isRoot()
+  {
+    return this.type == ApiQuery.ROOT ? true : false;
+  }
+
+  /**
+   * isItem
+   *
+   * @since Mon 26 Aug 2019 03:21:49 PM CEST
+   */
+  isItem()
+  {
+    return this.type == ApiQuery.ITEM ? true : false;
+  }
+}
---
+++ src/QueryBuilder.js
@@ -0,0 +1,138 @@
+import ApiQuery from './ApiQuery';
+
+/**
+ * Class: QueryBuilder
+ *
+ * Builds queries for JSON+Collection APIs
+ *
+ * @author S. Fleming <nuxnik@int5.net>
+ * @since Mon 26 Aug 2019 01:46:03 PM CEST
+ */
+export default class QueryBuilder
+{
+  /**
+   * The class constructor
+   */
+  constructor()
+  {
+    /**
+     * The current index
+     *
+     * @var number
+     */
+    this.index = 0;
+
+    /**
+     * The queries array
+     *
+     * @var array
+     */
+    this.queries = [];
+  }
+
+  /**
+   * increment the index
+   *
+   * @return QueryBuilder
+   */ 
+  next()
+  {
+    this.index++;
+
+    return this;
+  }
+
+  /**
+   * Decrement the index
+   *
+   * @return QueryBuilder
+   */
+  prev()
+  {
+    this.index--;
+
+    return this;
+  }
+
+  /**
+   * Reset the index
+   *
+   * @return QueryBuilder
+   */
+  resetIndex()
+  {
+    this.index = 0;
+
+    return this;
+  }
+
+  /**
+   * Get the root node query object
+   *
+   * @param {String} node The rel/node name
+   * @return ApiQuery
+   */
+  root(node)
+  {
+    let query = this.getQuery().setType(ApiQuery.ROOT).setNode(node);
+    this.next();
+    return query;
+  }
+
+  /**
+   * Get the item node query object
+   *
+   * @param {String} node The rel/node name
+   * @return ApiQuery
+   */
+  item(node)
+  {
+    let query = this.getQuery().setType(ApiQuery.ITEM).setNode(node);
+    this.next();
+    return query;
+  }
+
+  /**
+   * Get the current query object. Alias of getQuery
+   *
+   * @return ApiQuery
+   */
+  current()
+  {
+    return this.getQuery();
+  }
+
+  /**
+   * Get the current query object.
+   *
+   * @return ApiQuery
+   */
+  getQuery()
+  {
+    if (this.queries[this.index] == undefined) {
+      this.queries[this.index] = new ApiQuery();
+    }
+
+    return this.queries[this.index];
+  }
+
+  /**
+   * isQuery
+   *
+   * @since Mon 26 Aug 2019 03:52:13 PM CEST
+   */
+  isQuery()
+  {
+    return this.queries[this.index] ? true : false;
+  }
+
+  /**
+   * Get array of query objects
+   * 
+   * @return array
+   */
+  getQueries()
+  {
+    return this.queries;
+  }
+}
